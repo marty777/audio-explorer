6329e8f8692f2da12f27953b51b8ccc2b03d81cc
Martin Thorne <martin.thorne@gmail.com>
2020-07-30 13:58:59 -0600

    signal processing scaffolding

---
+++ AudioExplorer/SampleProcessor/SignalProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSCore;
+
+namespace AudioExplorer.SampleProcessor
+{
+    /// <summary>
+    /// Takes input from one or more iReadableAudioSources and performs operations on them
+    /// before producing a single audio outputs. The base class for filters, mixing, etc.
+    /// </summary>
+    public interface SampleProcessor : IReadableAudioSource<float>
+    {
+    }
+}
